725d23ed999a6032b1a20146eeca04cf576dfc2b
Richa.Kalani <Richa.Kalani@ril.com>
2023-01-09 20:50:20 +0530

    Initial Commit

---
+++ The Sports App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Sports App
+//
+//  Created by Richa Kalani on 09/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
