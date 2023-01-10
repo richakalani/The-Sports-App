1bd9497d3a056242195c3bbd13efc0f311986baa
Richa.Kalani <Richa.Kalani@ril.com>
2023-01-10 20:24:09 +0530

    screen 1 done

--- The Sports App/TableViewCell.swift
+++ The Sports App/TableViewCell.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
     @IBOutlet weak var teamName: UILabel!
+    @IBOutlet weak var venue: UILabel!
+    @IBOutlet weak var dateTime: UILabel!
     let networkManager = SportsManagerNetwork()
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -17,10 +19,18 @@ class TableViewCell: UITableViewCell {
     
     func loadData() {
         networkManager.getData(completion: {(topLevel) in
+            
             DispatchQueue.main.async {
                 self.teamName.text = ("\(topLevel.teams["4"]?.nameFull ?? "") vs \(topLevel.teams["5"]?.nameFull ?? "")")
+                self.venue.text = topLevel.matchdetail.venue.name
+                self.dateTime.text = ("\(topLevel.matchdetail.match.date)  \(topLevel.matchdetail.match.time)")
             }
         })
+    }
+    
+    
+    @IBAction func moreDetails(_ sender: Any) {
+        
     }
     
 }
